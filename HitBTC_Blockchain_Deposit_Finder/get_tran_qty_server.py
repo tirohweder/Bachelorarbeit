80dfe0753b325f4bf76cab5a98017091b4c09294
Timm Rohweder <trohwede@mail.uni-mannheim.de>
2022-06-06 23:31:07 +0200

    find qty api

--- HitBTC_Blockchain_Deposit_Finder/get_tran_qty_server.py
+++ HitBTC_Blockchain_Deposit_Finder/get_tran_qty_server.py
@@ -92,13 +92,15 @@ def find_qty2(cur, con, cur2):
         edited= json.loads(response.text)
 
         value= 0
-        for i in edited["out"]:
-            try:
-                if i["addr"] == address:
-                     value= value + int(i["value"])
-            except Exception:
-                pass
-
+        try:
+            for i in edited["out"]:
+                try:
+                    if i["addr"] == address:
+                         value= value + int(i["value"])
+                except Exception:
+                    pass
+        except Exception:
+            pass
 
         #print(txid, address, value)
         statement = "UPDATE depositing_transactions " \
